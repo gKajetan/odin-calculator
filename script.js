03bf2d5e020cec01609d5b5113a39fbfbcf6097c
Kajetan <147710182+gKajetan@users.noreply.github.com>
2025-12-17 18:11:47 +0100

    Proper calculator logic rebuild

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-console.log('Calculator Init')
+console.log('Calculator Init');
 
+// math functions
 function add(a, b) {
     return a + b;
 }
@@ -16,24 +17,74 @@ function divide(a, b) {
     return a / b;
 }
 
-let result = '0';
+// calculator vars
+let firstNumber = '';   // first/previously inputted number remembered by var
+let currentNumber = ''; // currently inputting number, used with previous/first number
+let currentOperator = null; // operator
 
-function updateScreen() {
-    document.getElementById('result').textContent = result;
+// update screen content
+function updateScreen(value) {
+    document.getElementById('result').textContent = value;
 }
 
-function clearScreen() {
-    result = 0;
-    updateScreen();
-}
-
+// button logic (arguments are taken from html's onclick)
 function printNumber(value) {
-    if (result === '0') {
-        result = value;
-    } 
-    else {
-        result += value;
+    // IF clicked number OR dot
+    if (!isNaN(value) || value === '.') {
+        if (currentOperator === null) {
+            // no operator = building first number
+            firstNumber += value;
+            updateScreen(firstNumber);
+        }
+        else {
+            // IF there is operator, start building next number
+            currentNumber += value;
+            updateScreen(currentNumber);
+        }
     }
-    updateScreen();
+
+    // IF clicked operator
+    else if (['+', '-', '*', '/'].includes(value)) {
+        // eval expression IF there is firstNumber, operator and currentNumber
+        if (firstNumber !== '' && currentNumber !== '') {
+            firstNumber = operate(currentOperator, firstNumber, currentNumber).toString();
+            currentNumber = '';
+            updateScreen(firstNumber);
+        }
+        currentOperator = value;
+    }
+    // IF '=' is clicked, display result
+    else if (value === '=') {
+        if (currentOperator !== null && currentNumber !== '') {
+            firstNumber = operate(currentOperator, firstNumber, currentNumber).toString();
+            currentNumber = '';
+            currentOperator = null;
+            updateScreen(firstNumber);
+        }
+    }
+}
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return "ERROR"; // dividing by zero
+            return divide(a, b);
+    }
+}
+
+// clear/reset screen 
+function clearScreen() {
+    firstNumber = '';
+    currentNumber = '';
+    currentOperator = null;
+    updateScreen('0');
 }
 
